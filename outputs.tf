575f7b15aa5409dd6b92195d243ca24f670e4c0c
GiuseppeTT <giu.tinti@gmail.com>
2023-02-13 12:17:57 -0800

    Dump changes

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "resource_group_name" {
+  value       = azurerm_resource_group.this.name
+  description = "The resource group's name"
+}
+
 output "database_username" {
   value       = azurerm_postgresql_flexible_server.this.administrator_login
   description = "The database's username"
@@ -12,6 +17,22 @@ output "database_password" {
 output "database_fqdn" {
   value       = azurerm_postgresql_flexible_server.this.fqdn
   description = "The database's FQND"
+}
+
+output "container_registry_username" {
+  value       = azurerm_container_registry.this.admin_username
+  description = "The container registry's username"
+}
+
+output "container_registry_password" {
+  value       = azurerm_container_registry.this.admin_password
+  description = "The container registry's password"
+  sensitive   = true
+}
+
+output "container_registry_fqdn" {
+  value       = azurerm_container_registry.this.login_server
+  description = "The container registry's FQND"
 }
 
 output "app_fqdn" {
